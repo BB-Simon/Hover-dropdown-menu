d44c503bb96656836cdf36ef13f3011043b6c1f1
BB-Simon <simon.chowdery@gmail.com>
2019-12-29 02:38:31 -0800

    hover dropdown menu

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.has-dropdown').mouseenter(function () {
+        var $this = $(this);
+        $this
+            .find('.dropdown')
+            .css('display', 'block')
+            .addClass('animated-fast fadeInUpMenu');
+
+    }).mouseleave(function () {
+        var $this = $(this);
+
+        $this
+            .find('.dropdown')
+            .css('display', 'none')
+            .removeClass('animated-fast fadeInUpMenu');
+    });
+});
